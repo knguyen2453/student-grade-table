ca10d3b915320b9fd11848802c2fffa0dd5a01a3
knguyen2453 <yeaitskevinn@yahoo.com>
2020-06-19 00:59:54 -0700

    view-average-grade

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 var tableElement = document.getElementsByClassName("table");
 var gradeTable = new GradeTable(tableElement);
 
-var app = new App(gradeTable);
+var headerElement = document.querySelector("header");
+var pageHeader = new PageHeader(headerElement);
+
+var app = new App(gradeTable, pageHeader);
 
 app.start();
