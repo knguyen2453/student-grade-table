ae050964417b1c4d92e94a67f2608cd9656f6c81
knguyen2453 <yeaitskevinn@yahoo.com>
2020-06-27 10:24:56 -0700

    delete-a-grade

--- components/grade-table.js
+++ components/grade-table.js
@@ -1,40 +1,27 @@
 class GradeTable {
-  constructor(tableElement) {
+  constructor(tableElement, noGradesElement) {
     this.tableElement = tableElement;
+    this.noGradesElement = noGradesElement;
   }
   updateGrades(grades) {
-    this.tableElement = document.querySelector("tbody");
-    for (var i = 0; i < grades.length; i++) {
+    if (grades === " ") {
+      var pElement = document.querySelector("p");
+      pElement.removeClass();
+        } else {
+      this.tableElement = document.querySelector("tbody");
+        for (var i = 0; i < grades.length; i++) {
       var studentGrades = grades[i];
 
-      var tableRow = document.createElement("tr");
-
-      var name = document.createElement("td");
-      name.textContent = studentGrades.name;
-
-      var course = document.createElement("td");
-      course.textContent = studentGrades.course;
-
-      var grade = document.createElement("td");
-      grade.textContent = studentGrades.grade;
-
-      var operations = document.createElement("td");
-
-      tableRow.append(name, course, grade, operations);
+      const tableRow = this.renderGradeRow(studentGrades, this.deleteGrade);
 
       this.tableElement.append(tableRow);
-
-      this.renderGradeRow(tableRow, this.deleteGrade);
+      }
     }
   }
   onDeleteClick(deleteGrade) {
     this.deleteGrade = deleteGrade;
   }
-  renderGradeRow(grades, deleteGrade) {
-    this.tableElement = document.querySelector("tbody");
-
-    var studentGrades = grades[i];
-
+  renderGradeRow(studentGrades, deleteGrade) {
     var newTableRow = document.createElement("tr");
 
     var newName = document.createElement("td");
@@ -50,7 +37,6 @@ class GradeTable {
 
     var deleteButton = document.createElement("button");
     deleteButton.innerHTML = "DELETE";
-    deleteButton.style.class = "btn btn-danger";
     deleteButton.addEventListener("click", function() {
       deleteGrade(grades.id);
     });
@@ -59,6 +45,6 @@ class GradeTable {
 
     newTableRow.append(newName, newCourse, newGrade, newOperations);
 
-    this.tableElement.append(newTableRow);
+    return newTableRow;
   }
 };
--- main.js
+++ main.js
@@ -1,5 +1,7 @@
+var noGrades = document.querySelector("p");
+
 var tableElement = document.getElementsByClassName("table");
-var gradeTable = new GradeTable(tableElement);
+var gradeTable = new GradeTable(tableElement, noGrades);
 
 var headerElement = document.querySelector("header");
 var pageHeader = new PageHeader(headerElement);
@@ -9,4 +11,4 @@ var gradeForm = new GradeForm(formElement);
 
 var app = new App(gradeTable, pageHeader, gradeForm);
 
-app.start();
+app.start(this.gradeTable.onDeleteClick(this.deleteGrade));
